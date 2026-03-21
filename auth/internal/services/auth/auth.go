4b73147048d8147ba06854f2a4c10ac92e7cce85
Артём Цыбенко <artemcybenko@192.168.0.14>
2026-03-21 23:32:14 +0700

    add grpc, services in auth

---
+++ auth/internal/services/auth/auth.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"auth/internal/domain/models"
+	"context"
+	"log/slog"
+	"time"
+)
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+func New(log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL, // Время жизни возвращаемых токенов
+	}
+}
